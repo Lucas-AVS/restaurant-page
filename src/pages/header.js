7bf65a6ec9eb41b697b33665408e8894c7dd347a
Ponk8 <lucas.alencar31@gmail.com>
2024-03-12 21:28:06 -0300

    feat: Create header component and its styles

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,5 +1,29 @@
-export default function header() {
-  let div = document.createElement("header");
-  div.innerHTML = "<p>This is a header</p>";
-  document.body.appendChild(div);
+import "../styles/header.css";
+
+export default function createHeader() {
+  const header = document.createElement("header");
+  const div = document.createElement("div");
+  div.className = "container";
+  const img = document.createElement("img");
+  img.src = "/src/assets/capizzabar-logo.png";
+
+  const ul = document.createElement("ul");
+
+  const homeLi = document.createElement("li");
+  homeLi.textContent = "HOME";
+  const menuLi = document.createElement("li");
+  menuLi.textContent = "MENU";
+  const aboutLi = document.createElement("li");
+  aboutLi.textContent = "ABOUT";
+
+  ul.appendChild(homeLi);
+  ul.appendChild(menuLi);
+  ul.appendChild(aboutLi);
+
+  div.appendChild(img);
+  div.appendChild(ul);
+
+  header.appendChild(div);
+
+  document.body.appendChild(header);
 }
