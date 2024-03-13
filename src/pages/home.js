d47a350b097b3cc8d47c6ed0035be4d5126059b8
Ponk8 <lucas.alencar31@gmail.com>
2024-03-12 21:27:16 -0300

    feat: Create home page and apply styles

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,33 @@
-import header from "./header";
+import "../styles/home.css";
 
 export default function home() {
-  header();
+  const main = document.createElement("main");
+  const ImgDiv = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "/src/assets/bar-entrance.png";
+  const address = document.createElement("p");
+  const TextDiv = document.createElement("div");
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+
+  address.textContent = "123 Neon Lane, Lumina City.";
+  p1.textContent =
+    "The Capybara brothers, renowned for their quirky creativity, have woven enchantment into every corner of CaPizzaBar. ";
+  p2.textContent =
+    "Step into CaPizzaBar, a neon-lit oasis crafted by the imaginative Capybara brothers. Their pizzas are an art form—crispy crusts, magical toppings, and cheese that sings. Pair your slice with signature cocktails that glow under blacklight.";
+
+  ImgDiv.className = "image-div";
+  TextDiv.className = "text-div";
+  main.className = "home-main";
+
+  TextDiv.appendChild(p1);
+  TextDiv.appendChild(p2);
+
+  ImgDiv.appendChild(img);
+  ImgDiv.appendChild(address);
+
+  main.appendChild(ImgDiv);
+  main.appendChild(TextDiv);
+
+  document.body.appendChild(main);
 }
