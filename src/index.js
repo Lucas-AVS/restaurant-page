3214f81631d9e95d15659f17e7ac2843dfbb5eb1
Ponk8 <lucas.alencar31@gmail.com>
2024-03-13 17:27:19 -0300

    feat: Reconstruct index.js to activate components and enable page navigation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import "./styles/home.css";
-import "./styles/menu.css";
-import "./styles/about.css";
-import home from "./pages/home";
-import menu from "./pages/menu";
-import about from "./pages/about";
+import "./styles/global.css";
 
-home();
+import firstLoad from "./functions/firstLoad.js";
+import changePage from "./functions/changePage.js";
+import resetPage from "./functions/resetPage.js";
+
+firstLoad();
+
+const home = document.querySelector(".home");
+const about = document.querySelector(".about");
+const menu = document.querySelector(".menu");
+
+function handleChangePage(page) {
+  return function () {
+    resetPage();
+    changePage(page);
+  };
+}
+
+home.addEventListener("click", handleChangePage("home"));
+about.addEventListener("click", handleChangePage("about"));
+menu.addEventListener("click", handleChangePage("menu"));
